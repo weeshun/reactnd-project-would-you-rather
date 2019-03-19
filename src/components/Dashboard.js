aede4f80286a1942b9912bdb85ada1d95fab676d
weeshun <weeshun@gmail.com>
2019-03-19 13:57:47 -0700

    Dashboard shows either answered questions or unanswered questions depending on its state variable showUnanswered

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,15 +8,22 @@ class Dashboard extends Component {
   }
 
   render() {
+    //const whatToShow = this.state.showUnanswered ? "Unanswered" : "Answered"
+    const selectedQuestions = this.state.showUnanswered
+                            ? this.props.unanswered
+                            : this.props.answered
+    //
+    //this.props.answered.map((id) =>
     return (
       <div>
+        <h5>Showing {this.state.showUnanswered ? "Unanswered" : "Answered"} questions</h5>
         <ul className='dashboard-list'>
-          {this.props.questionIds.map((id) => (
+          {Object.keys(selectedQuestions).map((id) => (
             <li key={id}>
               {/*
-                {this.store.questions[id].optionOne.text}
-                {id}
+                {this.props.questions[id].optionOne.text}
               */}
+              {id}
               <Question id={id}/>
             </li>
           ))}
@@ -28,22 +35,56 @@ class Dashboard extends Component {
 
 function mapStateToProps ({ authedUser, users, questions }) {
   const user = users[authedUser]
-  //const user = users.authedUser
   console.log('user: ', user)
-  //console.log('user.questions: ', user[questions])
-  //console.log('user.questions: ', user.questions)
-  //console.log('keys: ', Object.keys(user))
-  //console.log('user.id: ', user.id)
-  //const answeredQuestionIDs = users[authedUser].questions
-  // const answeredQuestionIDs = user.questions
-  // const unansweredQuestionIDs = Object.keys(questions).filter((id) => !answeredQuestionIDs.includes(id))
-  // console.log('*** authedUser', authedUser)
-  // console.log('answeredQuestionIDs:   ', answeredQuestionIDs)
-  // console.log('unansweredQuestionIDs: ', unansweredQuestionIDs)
+  //let selectedQuestions = {}
+  let answered = {}
+  let unanswered = {}
+  //if (typeof user !== 'undefined') {  // or loading has completed
+  if (typeof users[authedUser] !== 'undefined') {
+    //const answeredQuestionIDs = user.questions
+    // const answeredQuestionIDs = users[authedUser].questions
+    // const unansweredQuestionIDs = Object.keys(questions).filter((id) => !answeredQuestionIDs.includes(id))
+    const answeredIDs = users[authedUser].questions
+    const unansweredIDs = Object.keys(questions).filter((id) => !answeredIDs.includes(id))
+    // const selectedQuestionIDs = this.state.showUnanswered
+    //       ? Object.keys(questions).filter((id) => !answeredQuestionIDs.includes(id))
+    //       : users[authedUser].questions
+    //answeredQuestions = Object.assign({}, )
+    console.log("*** (1) ***")
+    // console.log("user.id: ", user.id)
+    console.log("answeredIDs: ", answeredIDs)
+    console.log("unansweredIDs: ", unansweredIDs)
+    // const obj1 = Object.assign({}, unansweredQuestionIDs)
+    // console.log("obj1: ", obj1)
+    // const obj2 = {...unansweredQuestionIDs}
+    // console.log("obj2: ", obj2)
+    //selectedQuestionIDs.forEach((id) => (selectedQuestions[id] = questions[id]))
+    //console.log("selectedQuestions: ", selectedQuestions)
+    answeredIDs.forEach((id) => (answered[id] = questions[id]))
+    unansweredIDs.forEach((id) => (unanswered[id] = questions[id]))
+    Object.keys(answered).sort((a, b) => answered[b].timestamp - answered[a].timestamp)
+    Object.keys(unanswered).sort((a, b) => unanswered[b].timestamp - unanswered[a].timestamp)
+    console.log("answered: ", answered)
+    console.log("unanswered: ", unanswered)
+  }
+
+  // questionIds: Object.keys(questions)
+  //   .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+
+  // answered: {
+  //   questionIds: Object.keys(answered)
+  //     .sort((a, b) => answered[b].timestamp - answered[a].timestamp)
+  // }
+  // unanswered: {
+  //   questionIds: Object.keys(unanswered)
+  //     .sort((a, b) => unanswered[b].timestamp - unanswered[a].timestamp)
+  // }
+
   return {
-    questionIds: Object.keys(questions)
-      .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+    answered,
+    unanswered
   }
 }
 
 export default connect(mapStateToProps)(Dashboard)
+//export default connect()(Dashboard)
