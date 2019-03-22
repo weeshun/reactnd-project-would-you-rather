02449253e23dc1fa8b9b4e225005d031282ae24f
weeshun <weeshun@gmail.com>
2019-03-22 12:12:11 -0700

    Minor changes

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -4,8 +4,10 @@ export default function authedUser (state = null, action) {
   switch (action.type) {
     case SET_AUTHED_USER :
       return action.id
-    case UNSET_AUTHED_USER :
+
+    case UNSET_AUTHED_USER :  // null initially; '' after signing out
       return ''
+
     default :
       return state
   }
